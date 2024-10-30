0addb6b377da15b7eab233363fb11b67eb2ed0a8
marcelarie <googlillo@gmail.com>
2024-10-30 03:34:46 +0100

    update

--- scripts/tofi/power-selector.sh
+++ scripts/tofi/power-selector.sh
@@ -1,24 +1,36 @@
 #!/usr/bin/env bash
 
-# pkill tofi or 
-if pgrep -x "tofi" > /dev/null
-then
-  pkill tofi
+# pkill tofi or
+if pgrep -x "tofi" >/dev/null; then
+	pkill tofi
 fi
 
-entries="Logout\nSuspend\nReboot\nShutdown"
+Lock
+Logout
+Reboot
+Shutdown
+Suspend
+
+entries="Lock\nLogout\nReboot\nShutdown\nSuspend"
 
 selected=$(echo -e "$entries" | tofi --width 250 --height 210 | awk '{print $1}')
 
 [[ -z $selected ]] && exit
 
 case $selected in
-  Logout)
-    hyprctl dispatch exit;;
-  Suspend)
-    exec systemctl suspend;;
-  Reboot)
-    exec systemctl reboot;;
-  Shutdown)
-    exec systemctl poweroff -i;;
+Lock)
+	hyprlock
+	;;
+Logout)
+	hyprctl dispatch exit
+	;;
+Suspend)
+	exec systemctl suspend
+	;;
+Reboot)
+	exec systemctl reboot
+	;;
+Shutdown)
+	exec systemctl poweroff -i
+	;;
 esac
