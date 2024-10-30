0d39251d820f23ff3c8b7309ae36459ca2352a86
marcelarie <googlillo@gmail.com>
2024-10-30 03:49:33 +0100

    add lock and suspend option to power selector

--- scripts/tofi/power-selector.sh
+++ scripts/tofi/power-selector.sh
@@ -1,36 +1,34 @@
 #!/usr/bin/env bash
 
-# pkill tofi or
 if pgrep -x "tofi" >/dev/null; then
 	pkill tofi
 fi
 
-Lock
-Logout
-Reboot
-Shutdown
-Suspend
+entries="Lock\nLock & Suspend\nLogout\nReboot\nShutdown\nSuspend"
 
-entries="Lock\nLogout\nReboot\nShutdown\nSuspend"
-
-selected=$(echo -e "$entries" | tofi --width 250 --height 210 | awk '{print $1}')
+selected=$(echo -e "$entries" | tofi --width 250 --height 270 | awk '{print $0}')
 
 [[ -z $selected ]] && exit
 
-case $selected in
-Lock)
+case "$selected" in
+"Lock")
 	hyprlock
 	;;
-Logout)
+"Lock & Suspend")
+	hyprlock &
+	disown && systemctl suspend
+	;;
+"Logout")
 	hyprctl dispatch exit
 	;;
-Suspend)
+"Suspend")
 	exec systemctl suspend
 	;;
-Reboot)
+"Reboot")
 	exec systemctl reboot
 	;;
-Shutdown)
+"Shutdown")
 	exec systemctl poweroff -i
 	;;
 esac
+
