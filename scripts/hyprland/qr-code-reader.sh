78ecde442c16a6e36ad38be98ea3bc8380d28eb7
marcelarie <googlillo@gmail.com>
2024-12-10 15:54:27 +0100

    update config

---
+++ scripts/hyprland/qr-code-reader.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+output=$(hyprshot --mode region --raw | zbarimg --raw - 2>&1)
+
+if [[ $? -ne 0 ]]; then
+	notify-send "Error" "QR scanned failed"
+	exit 1
+fi
+
+number_of_scans=$(echo "$output" | grep -oP 'scanned \K\d+')
+scanned_content=$(echo "$output" | awk '/scanned [0-9]+ barcode symbols from [0-9]+ images in [0-9.]+ seconds/{exit} {print}')
+
+if [[ $number_of_scans -ge 2 ]]; then
+	selected=$(echo "$scanned_content" | tofi)
+	echo "$selected" | wl-copy
+	notify-send "Copied to clipboard" "$selected"
+else
+	echo "$scanned_content" | wl-copy
+	notify-send "Copied to clipboard" "$scanned_content"
+fi
