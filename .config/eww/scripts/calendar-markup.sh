393b2caceb75d830b5cf23ff6f14e5d5d4e36ff8
marcelarie <googlillo@gmail.com>
2025-09-13 18:24:57 +0200

    update dots

--- .config/eww/scripts/calendar-markup.sh
+++ .config/eww/scripts/calendar-markup.sh
@@ -5,4 +5,4 @@ today=$(date +%d)
 
 # Get calendar output and process it
 cal | sed "s/\b${today}\b/<span color=\"#e6c446\" weight=\"bold\">${today}<\/span>/g"
-carl --year-progress | sed 's/\x1b\[[0-9;]*m//g'
+carl --year-progress | sed 's/\x1b\[[0-9;]*m//g' | sed -E 's/([0-9]+\.[0-9]+%)/<span color="#e6c446" weight="bold">\1<\/span>/g; s/([0-9]+) left/<span color="#ff6b6b" weight="bold">\1<\/span> left/g'
