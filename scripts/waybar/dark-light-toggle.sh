a7da16e85210657d216e14ad17c05f2b974346e3
themarcel <code@marcel.cool>
2026-05-13 16:53:29 +0200

    fix theme

--- scripts/waybar/dark-light-toggle.sh
+++ scripts/waybar/dark-light-toggle.sh
@@ -29,6 +29,7 @@ if [[ "$current" == "dark" ]]; then
     wallpaper="$HOME/img/dark-grey.png"
     hypr_active_border="rgba(33ccffee)"
     hypr_inactive_border="rgba(595959aa)"
+    hypr_locked_border="rgba(d8da9dff)"
 else
     icon="󰖨"
     color_scheme="prefer-light"
@@ -37,6 +38,7 @@ else
     wallpaper="$HOME/img/light-spring.png"
     hypr_active_border="rgba(ffffffff)"
     hypr_inactive_border="rgba(64666caa)"
+    hypr_locked_border="rgba(00008bff)"
 fi
 
 gsettings set org.gnome.desktop.interface color-scheme "$color_scheme"
@@ -45,6 +47,7 @@ gsettings set org.gnome.desktop.interface cursor-theme "$cursor_theme"
 hyprctl setcursor "$cursor_theme" 24 >/dev/null 2>&1
 hyprctl keyword general:col.active_border "$hypr_active_border" >/dev/null 2>&1
 hyprctl keyword general:col.inactive_border "$hypr_inactive_border" >/dev/null 2>&1
+hyprctl keyword windowrule "border_color $hypr_locked_border, match:tag locked" >/dev/null 2>&1
 
 sed -i "s/^initial-color-theme=.*/initial-color-theme=${current}/" "$HOME/.config/foot/foot.ini"
 if [[ "$current" == "dark" ]]; then
@@ -54,10 +57,12 @@ else
 fi
 
 ln -sf "style-colors-${current}.css" "$HOME/.config/waybar/style-colors.css"
+ln -sf "style-${current}.css" "$HOME/.config/swaync/style.css"
 
 if [[ "$1" == "toggle" ]]; then
     pkill -SIGUSR2 waybar 2>/dev/null
     pkill -RTMIN+12 waybar 2>/dev/null
+    swaync-client --reload-css 2>/dev/null
 
     for sock in /run/user/$(id -u)/nvim.*.0 ; do
         [ -S "$sock" ] && nvim --server "$sock" --remote-send "<Cmd>set background=${current}<CR>" 2>/dev/null &
