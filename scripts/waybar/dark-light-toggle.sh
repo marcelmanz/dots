2a0b629f55b0a563f9da576287423c40e1cdeb93
themarcel <code@marcel.cool>
2026-06-18 14:29:54 +0200

    update dots

--- scripts/waybar/dark-light-toggle.sh
+++ scripts/waybar/dark-light-toggle.sh
@@ -36,8 +36,8 @@ else
     gtk_theme="Adwaita"
     cursor_theme="retrosmart-xcursor-black-shadow"
     wallpaper="$HOME/img/light-spring.png"
-    hypr_active_border="rgba(ffffffff)"
-    hypr_inactive_border="rgba(64666caa)"
+    hypr_active_border="rgba(007f86ee)"
+    hypr_inactive_border="rgba(64666c55)"
     hypr_locked_border="rgba(00008bff)"
 fi
 
